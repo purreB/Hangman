78afd3a4323a707b7647b8f878ab67a50d926374
purreB <jakob.bergstrom@live.se>
2021-03-12 14:20:31 +0100

    done

--- Js/app.js
+++ Js/app.js
@@ -31,7 +31,7 @@ const wordList = [
 // Randomly choose word
 const randomWord = () => {
   const word = {
-    index: Math.floor(Math.random() * wordList.length)
+    index: Math.floor(Math.random() * wordList.length),
   };
   return word;
 };
@@ -40,11 +40,11 @@ const gameWord = randomWord();
 console.log(wordList[gameWord.index]);
 
 // IIFE
-( function() {
+(function () {
   for (let i = 0; i < wordList[gameWord.index].length; i++) {
     createP(i);
   }
-}());
+})();
 
 const allowedCharacters = [
   "a",
@@ -75,7 +75,7 @@ const allowedCharacters = [
   "z",
 ];
 
-function createP(index){
+function createP(index) {
   document.querySelector(".output").innerHTML += `
     <div class="wordLetter-wrapper">
       <p class="wordLetter wordLetter${index}"></p>
@@ -92,54 +92,67 @@ window.onsubmit = (e) => {
 
   // Fetch value of input field
   const inputValue = inputField.value.toLowerCase();
-  
+
   // Check if it is a valid character
   if (allowedCharacters.includes(inputValue)) {
-
     // loop through every index of the string gameWord
     // Compare to see if character exists in gameWord
-    for( let i = 0; i < wordList[gameWord.index].length; i++) {
-      if(inputValue === wordList[gameWord.index].charAt(i)) {
+    for (let i = 0; i < wordList[gameWord.index].length; i++) {
+      if (inputValue === wordList[gameWord.index].charAt(i)) {
         document.querySelector(`.wordLetter${i}`).innerText = inputValue;
-          ifGuessedRight = true;
-          break;
-        } else {
-          ifGuessedRight = false;
-        }
+        ifGuessedRight = true;
+        break;
+      } else {
+        ifGuessedRight = false;
       }
-      
-      // Add image
-    if(countImage === 0 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+
+    // Add image
+    if (countImage === 0 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
-    } 
-    if(countImage === 1 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+    if (countImage === 1 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
-    } 
-    if(countImage === 2 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+    if (countImage === 2 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
-    } 
-    if(countImage === 3 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+    if (countImage === 3 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
-    } 
-    if(countImage === 4 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+    if (countImage === 4 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
-    } 
-    if(countImage === 5 && ifGuessedRight === false) {
-      $(`.game-image[src='./Images/${countImage}.png'`).classList = notInvisibleClass;
+    }
+    if (countImage === 5 && ifGuessedRight === false) {
+      $(
+        `.game-image[src='./Images/${countImage}.png'`
+      ).classList = notInvisibleClass;
       countImage++;
       ifGuessedRight = true;
       countImage = 0;
-      document.querySelector(".output").innerHTML = `<p style="font-size:5rem;color:red;">You lost! Try again!</p>`;
+      document.querySelector(
+        ".output"
+      ).innerHTML = `<p style="font-size:5rem;color:red;">You lost! Try again!</p>`;
     }
   }
   return false;
